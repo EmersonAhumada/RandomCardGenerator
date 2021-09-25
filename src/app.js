eae8dcf4b3832b9b2b427ab09311f0f5e3c8533c
EmersonAhumada <emerson.ahumada@gmail.com>
2021-09-25 13:35:22 +0000

    two thirds

--- src/app.js
+++ src/app.js
@@ -5,7 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector(".card").innerHTML = randomnumber();
+  document.querySelector(".card").classList.add(randomtype());
 };
+
+let randomnumber = () => {
+  let num = ["A", "J", "K", "Q", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+  let randomnum = Math.floor(Math.random() * num.length);
+  return num[randomnum];
+};
+
+let randomtype = () => {
+  let tipo = ["diamante", "corazon", "pica", "trebol"];
+  let randomtipo = Math.floor(Math.random() * tipo.length);
+  return tipo[randomtipo];
+};
+
+setInterval(window.onload, 10000);
